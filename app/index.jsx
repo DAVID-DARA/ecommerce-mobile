77823b3f3fbd4769cad03bdf48ab93ddd8254a3d
DAVID-DARA <davidfamoyegun1@gmail.com>
2024-10-10 16:14:57 +0100

    Initial Commit base-code

---
+++ app/index.jsx
@@ -0,0 +1,22 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
